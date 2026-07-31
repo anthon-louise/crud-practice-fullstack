1e9bcdf471653159de2d2d56a39e9a41bba332ae
anthon-dev <anthonlouisenoynay123@example.com>
2026-07-31 17:24:27 +0800

    db connection and error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorHandler.js";
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(errorHandler);
+
 app.listen(() => {
-  console.log(`Server running on port: ${port}`)
+  console.log(`🚀Server running on port: ${port}`)
 })
