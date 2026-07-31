b38458ff98f8ced70e6399c7b4ced258242efc49
anthon-dev <anthonlouisenoynay123@example.com>
2026-07-31 17:38:23 +0800

    create note backend

---
+++ backend/src/module/note/controller.ts
@@ -0,0 +1,21 @@
+import asyncHandler from "express-async-handler";
+import { Note } from "./type.js";
+import { createNoteSchema, updateNoteSchema } from "./schema.js";
+import { pool } from "../../config/db.js";
+import { AppError } from "../../errors/AppError.js";
+import { Request, Response } from "express";
+
+export const createNote = asyncHandler(async (req: Request, res: Response) => {
+  const {title, content} = createNoteSchema.parse(req.body);
+
+  await pool.query(`
+    INSERT INTO
+    notes (title, content)
+    VALUES ($1, $2)
+    `, [title, content]);
+
+  res.status(201).json({
+    message: "Note created"
+  });
+  
+});
